1b4ca1bb594e8125f501dd7a5398b0da75961106
Uttam <uttam002600@gmail.com>
2023-12-17 00:39:18 +0530

    Portfolio Added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings:["","Web Designer","Web developer","Graphic Designer","youtuber"],
+    typeSpeed:100,
+    backspeed:60,
+    loop:true
+});
